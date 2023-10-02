76cfe01485056b2f4d805cf4a647205d6bf5abf5
Ayari Hedi <mohamedhedi.ayari@esprit.tn>
2023-10-02 15:29:36 +0200

    modification 2

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -18,7 +18,7 @@ import javax.persistence.TemporalType;
 @Table(name = "T_USER")
 public class User implements Serializable {
 	//Test
-
+	//Test2Modif
 	private static final long serialVersionUID = 1L;
 
 	@Id
